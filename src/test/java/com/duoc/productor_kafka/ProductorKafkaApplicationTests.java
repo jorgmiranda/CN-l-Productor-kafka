3378ab4e9e4ddd37d0f9cb0e71e61b98112ff0c9
jorge-miranda <jorge.miranda@mysolutions.cl>
2025-02-12 18:15:57 -0300

    Primer commit

---
+++ src/test/java/com/duoc/productor_kafka/ProductorKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.productor_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductorKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
